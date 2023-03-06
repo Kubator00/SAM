1baa4d5a3d579928b09db7e07402414f5660bcd8
Jakub Więcek <71319403+Kubator00@users.noreply.github.com>
2023-03-06 15:17:40 +0100

    update

--- lab1/source/server.js
+++ lab1/source/server.js
@@ -3,7 +3,25 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-    res.send('Hello World!')
+    res.send(`<body>
+
+<form method="get">
+
+<input type="video" id="audioPlayer"/>
+<input type="audio" id="videoPlayer"/>
+</form>
+
+
+</body>
+
+<script>
+
+const queryString = window.location.search;
+const urlParams = new URLSearchParams(queryString);
+
+const videoFiles = urlParams.get('videoFile')
+
+</script>`)
 })
 
 app.listen(4080)
