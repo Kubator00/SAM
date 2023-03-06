65a7eea995894c2e16baf186da8327e6e8391104
Jakub Więcek <71319403+Kubator00@users.noreply.github.com>
2023-03-06 15:38:35 +0100

    update

--- lab1/source/server.js
+++ lab1/source/server.js
@@ -6,9 +6,9 @@ app.get('/', (req, res) => {
     const videoFiles = req.query.videoFiles;
     const audioFiles = req.query.audioFiles;
     if(videoFiles)
-    	res.send(`<body> <input type="video" id="videoPlayer"/></body>`)
+    	res.send(`<body> <input type="video" id="videoPlayer" value="${videoFiles}"/></body>`)
     else if(audioFiles)
-    	res.send(`<body> <input type="audio" id="videoPlayer"/></body>`)
+    	res.send(`<body> <input type="audio" id="audioPlayer" value="${audioFiles}"></body>`)
 })
 
 app.listen(4080)
