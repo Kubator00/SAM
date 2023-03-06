c703a167000f361d11ccb0c95142632726da00c9
Jakub Więcek <71319403+Kubator00@users.noreply.github.com>
2023-03-06 15:55:43 +0100

    update

--- lab1/source/server.js
+++ lab1/source/server.js
@@ -3,12 +3,20 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-    const videoFiles = req.query.videoFiles;
-    const audioFiles = req.query.audioFiles;
-    if(videoFiles)
+    const videoFiles = req.query.videoFile;
+    const audioFiles = req.query.audioFile;
+    if(videoFiles && audioFiles)
+       	res.send(`
+       	<body> 
+       	<input type="audio" id="audioPlayer" value="${audioFiles}">
+       	<input type="video" id="videoPlayer" value="${videoFiles}"/>
+       	</body>`)
+    else if(videoFiles)
     	res.send(`<body> <input type="video" id="videoPlayer" value="${videoFiles}"/></body>`)
     else if(audioFiles)
     	res.send(`<body> <input type="audio" id="audioPlayer" value="${audioFiles}"></body>`)
+    else
+    	res.send(`Hello`);
 })
 
 app.listen(4080)
