1ab9f415213fd0736306d80fd585660f0aca38c3
Jakub <jakub.wiecek@outlook.com>
2023-03-20 14:28:52 +0100

    poprawa lab1

--- lab2/source/server.js
+++ lab2/source/server.js
@@ -3,20 +3,34 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-    const videoFiles = req.query.videoFile;
-    const audioFiles = req.query.audioFile;
-    if(videoFiles && audioFiles)
-       	res.send(`
-       	<body> 
-       	<input type="audio" id="audioPlayer" value="${audioFiles}">
-       	<input type="video" id="videoPlayer" value="${videoFiles}"/>
-       	</body>`)
-    else if(videoFiles)
-    	res.send(`<body> <input type="video" id="videoPlayer" value="${videoFiles}"/></body>`)
-    else if(audioFiles)
-    	res.send(`<body> <input type="audio" id="audioPlayer" value="${audioFiles}"></body>`)
-    else
-    	res.send(`Hello`);
+	const videoFile = req.query.videoFile;
+	const audioFile = req.query.audioFile;
+	let page = `<!DOCTYPE HTML>
+    <html>
+    <head>
+    <title>Hello World Player</title>
+    </head>
+    <body>
+    `
+
+	if (videoFile)
+		page += `
+			<video controls id="videoPlayer">
+		<source src="${videoFile}" type="video/mp4">
+		Twoja przeglądarka nie obsługuje odtwarzacza wideo.
+		</video>`
+
+	if (audioFile)
+		page += `
+    	<audio controls id="audioPlayer">
+		<source src="${audioFile}" type="audio/mp3">
+		Twoja przeglądarka nie obsługuje odtwarzacza audio.
+		</audio>`
+
+
+	page += `</body> </html>`
+	res.send(page);
+
 })
 
 app.listen(4080)
