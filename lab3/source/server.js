186770812226e816f26f1cb6cd2034e4d00b17ba
Jakub <jakub.wiecek@outlook.com>
2023-04-03 15:21:58 +0200

    addRow fix

--- lab3/source/server.js
+++ lab3/source/server.js
@@ -9,7 +9,7 @@ const addVideoPlayer = (videoFile) => {
 		<source src="${videoFile}" type="video/mp4">
 		Twoja przeglądarka nie obsługuje odtwarzacza wideo.
 		</video><br/><br/>`
-    result += `<button id="videoCancel" onClick="cancelVideo()">Anulowanie filmu</button><br><br>`;
+    result += `<button id="videoCancel" onClick="cancelVideo()">Anulowanie filmu</button>`;
     result += `<button id="videoAdd" onClick="addRow('Video')">Add video</button><br><br>`;
     result += `
     <script>
@@ -30,7 +30,7 @@ const addAudioPlayer = (audioFile) => {
 		Twoja przeglądarka nie obsługuje odtwarzacza audio.
 		</audio><br/><br/>`
     result += `<button id="audioCancel" onClick="cancelAudio()">Anulowanie audio</button>`;
-    result += `<button id="addAudio" onClick="">Add audio</button><br><br>`;
+    result += `<button id="addAudio" onClick="addRow('Audio')">Add audio</button><br><br>`;
     result += `<script>
     function cancelAudio(){
         const audioPlayer = document.getElementById("audioPlayer");
@@ -44,28 +44,37 @@ const addAudioPlayer = (audioFile) => {
 
 const addImage = (imgFile) => {
     let result = `<img src=${imgFile} id="posterImage"/>`;
-    result += `<button id="imgAdd" onClick="">Add image</button><br><br>`;
+    result += `<button id="imgAdd" onClick="addRow('Image')">Add image</button><br><br>`;
     return result;
 }
 
-const addTable = (url,type) => {
+const addTable = (url, type) => {
     let result = `<table id='playlist_table'>`;
     result += `<tr><th>No.</th><th>URL</th><th>Type</th></tr>`;
     result += `</table>`
     result += `
     <script>
-        let rowNumber = 1;
-        function addRow(type){
-            let src;
-            if(type=='Video')
-                src=  document.getElementById('videoPlayer').scr;
-            
-            const table = document.getElementById('playlist_table');
-            const newRow = table.insertRow(-1); 
-   
-        
-            rowNumber++;
-        }
+    let rowNumber = 1;
+    function addRow(type) {
+        let src;
+        if (type == 'Video')
+            src = document.getElementById('videoPlayer').getAttribute('src');
+        else if (type == 'Audio')
+            src = document.getElementById('audioPlayer').getAttribute('src');
+        else if (type == 'Image')
+            src = document.getElementById('posterImage').getAttribute('src');
+
+        let table = document.getElementById('playlist_table');
+        let newRow = table.insertRow(-1);
+        let newRowNumber = newRow.insertCell(0);
+        newRowNumber.innerText = rowNumber;
+        let newRowSrc = newRow.insertCell(1);
+        newRowSrc.innerText = src;
+        let newRowType = newRow.insertCell(2);
+        newRowType.innerText = type;
+    
+        rowNumber++;
+    }
     </script>
     `
     return result;
@@ -100,14 +109,14 @@ app.get('/', (req, res) => {
         res.write(addImage(imgFile))
 
     res.write(addTable());
-    
+
     if (!videoFile && !audioFile && !imgFile)
         res.write(`Nie przekazano parametru`)
 
 
 
     res.write(`</body> </html>`);
-	res.end();
+    res.end();
 })
 
 app.listen(4080)
